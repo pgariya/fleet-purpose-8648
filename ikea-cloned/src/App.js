35bc2c70023ae530c9004c40b7721efbbdaa6e3f
pgariya <8192969855p@gmail.com>
2023-01-17 12:02:16 +0530

    react folder

---
+++ ikea-cloned/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+      <h1>Home Page</h1>
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ ikea-cloned/src/Pages/AllRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {  Routes } from 'react-router-dom'
+
+const AllRoutes = () => {
+  return (
+    
+<Routes>
+
+{/* <Route path='/' element={<anything/>}/> */}
+
+
+
+
+
+</Routes>
+  )
+}
+
+export default AllRoutes
