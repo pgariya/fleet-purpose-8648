fd0c0716f7ed28075fe4b0ed290fd95e6f4a8c0d
pgariya <8192969855p@gmail.com>
2023-01-19 21:52:36 +0530

    added some files

--- ikea-cloned/src/Pages/AllRoutes.jsx
+++ ikea-cloned/src/Pages/AllRoutes.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "../Components/Login";
 import Signup from "../Components/Signup";
+import DashBoard from './DashBoard'
+import ProductsList from './ProductsList'
+import SingleProductsList from './SingleProductsList'
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
+      <Route path='/dashboard' element={<DashBoard/>}/>
+ <Route path='/productslist' element={<ProductsList/>}/>
+ <Route path='/productslist/:id' element={<SingleProductsList/>}/>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
--- ikea-cloned/src/Pages/ProductsList.jsx
+++ ikea-cloned/src/Pages/ProductsList.jsx
@@ -1,6 +1,5 @@
 
 
-
 import { Box, Button, Card, CardBody,  Heading, HStack, Image, SimpleGrid, Skeleton} from '@chakra-ui/react'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -25,44 +24,12 @@ let getmydata= async () =>{
         setisloading(false)
     }catch(err){
         seterror(err.message)
-
-import {
-  Box,
-  Card,
-  CardBody,
-  Heading,
-  HStack,
-  Image,
-  SimpleGrid,
-  Skeleton,
-} from "@chakra-ui/react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-
-import React, { useEffect, useState } from "react";
-
-const ProductsList = () => {
-  let [mydata, setmydata] = useState([]);
-  let [error, seterror] = useState("");
-  let [isloading, setisloading] = useState(false);
-
-  let getmydata = async () => {
-    try {
-      setisloading(true);
-      let res = await axios.get(`http://localhost:8080/real_products`);
-      setmydata(res.data);
-      setisloading(false);
-    } catch (err) {
-      seterror(err.message);
-
     }
-  };
+}
 
-  useEffect(() => {
-    getmydata();
-  }, []);
-
-  // console.log(mydata)
+useEffect(() =>{
+    getmydata()
+},[])
 
 
      // filter by clicking any of these 
@@ -118,16 +85,8 @@ let handleChildrenData = () =>{
 }
 
 
-  if (isloading) {
-    return (
-      <HStack m="auto" w="80%" mt={30}>
-        <Skeleton height="600px" w="600px" />
-        <Skeleton height="600px" w="600px" />
-      </HStack>
-    );
-  }
 
-
+if(isloading){
   return (
 
 <HStack m='auto' w='80%' mt={30}>
@@ -204,33 +163,8 @@ updatedData.map((el) => (
 
 
 
-
-    <Box bg={"gray.100"}>
-      {error !== "" && <Heading> Error: {error}</Heading>}
-
-      <Box w="90%" m="auto" mt="100px">
-        <SimpleGrid
-          columns={{ base: 1, md: 2, lg: 3 }}
-          gap={5}
-          textAlign="left"
-        >
-          {mydata.map((el) => (
-            <Link to={`/productslist/${el.id}`}>
-              <Card borderRadius="10px" bg={"white"} key={el.id}>
-                <CardBody>
-                  <Image src={el.img} alt="croww" borderRadius={10} w="100%" />
-                  <Heading fontSize="22px" fontWeight="bold" my={5}>
-                    {el.main_title}
-                  </Heading>
-                </CardBody>
-              </Card>
-            </Link>
-          ))}
-        </SimpleGrid>
-      </Box>
-
     </Box>
-  );
-};
+  )
+}
 
-export default ProductsList;
+export default ProductsList
