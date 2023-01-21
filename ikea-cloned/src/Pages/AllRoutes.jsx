b21b9ccc74891fe1959c6fd942f7da160ad17633
sumitbadri2000 <sumitbadri2000@gmail.com>
2023-01-21 13:10:50 +0530

    add

--- ikea-cloned/src/Pages/AllRoutes.jsx
+++ ikea-cloned/src/Pages/AllRoutes.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import DashBoard from "./DashBoard";
 import ProductsList from "./ProductsList";
 import SingleProductsList from "./SingleProductsList";
 import AdminLogin from "./adminLogin";
 import AdminDash from "./AdminDash";
 import AdminAdd from "./AdminAdd";
 import AdminManage from "./AdminManager";
-import Login from "../Components/Login";
-import Signup from "../Components/Signup";
 import CartItemsPage from "./CartItemsPage";
 import DashBoard from './DashBoard'
-import ProductsList from './ProductsList'
-import SingleProductsList from './SingleProductsList'
-
 import SingleSofaProducts from "./SingleSofaProducts";
 import SofaProducts from "./SofaProducts";
-
-import AdminLogin from "./adminLogin";
-import AdminDash from "./AdminDash";
 import FrontPage from "../Components/FrontPage";
 const AllRoutes = () => {
   return (
@@ -30,22 +21,13 @@ const AllRoutes = () => {
         <Route path="/AdminLogin" element={<AdminLogin />} />
         <Route path="/AdminDash" element={<AdminDash />} />
         <Route path="/Adminadd" element={<AdminAdd />} />
-        <Route path="/AdminManage" element={<AdminManage /
+        <Route path="/AdminManage" element={<AdminManage />}/>
         <Route path="/" element={<FrontPage/>}/>
-       <Route path='/dashboard' element={<DashBoard/>}/>
        <Route path='/productslist' element={<ProductsList/>}/>
        <Route path='/productslist/:id' element={<SingleProductsList/>}/>
-        <Route path="/login" element={<Login />} />
-
-       <Route path="/signup" element={<Signup />} />
         <Route path="/sofaproducts" element={<SofaProducts/>}/>
         <Route path='/sofaproducts/:id' element={<SingleSofaProducts/>}/>
         <Route path="/cartitemspage" element={<CartItemsPage/>}/>
-
-       
-              <Route path="/AdminLogin" element={<AdminLogin />} />
-      <Route path="/AdminDash" element={<AdminDash />} />
-
       </Routes>
     </div>
   );
